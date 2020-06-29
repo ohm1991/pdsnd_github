1a05fd232d7fd62b7d8eb7b0b87172317995352b
Omar Mawlawi <o.mawlawi06@gmail.com>
2020-06-28 21:23:29 -0400

    Changed variable name "res" to "response_next5rows"

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -178,12 +178,12 @@ def user_stats(df):
 
 def display_data(df, index):
     prompt = '\nDisplay next 5 rows of raw data (yes/no)? \n'
-    res = input(prompt).lower()
-    if res == 'yes':
+    response_next5rows = input(prompt).lower()
+    if response_next5rows == 'yes':
         print(df.iloc[index:index+5])
         index += 5
         return display_data(df, index)
-    if res == 'no':
+    if response_next5rows == 'no':
         return
     else:
         print("\nI'm sorry, I'm not sure if you wanted to see more data or not. Let's try again.")
